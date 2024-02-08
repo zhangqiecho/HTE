9048100e7688fea4bfa8b0991458ff58d9e0294a
Qi Zhang <qi.zhang0305@gmail.com>
2024-02-08 18:47:41 -0500

    Add files necessary to run the simulation study

---
+++ code/cluster_sim_func.R
@@ -0,0 +1,397 @@
+## FOCUS ON PROBLEM OF USING CF AND DR LEARNER TO FIND THE RIGHT MODIFIER
+# function of the simulation
+# including data generation and analysis
+## update in Jan 2024: 
+# 1) round down the continuous confounders
+
+# CREATE EXPIT AND LOGIT FUNCTIONS
+expit <- function(x){ exp(x)/(1+exp(x)) }
+logit <- function(x){ log(x/(1-x)) }
+
+cluster_sim <- function(seed_value, number_sims, sample_size, c_number, param1, param2, group){
+  
+  set.seed(seed_value)
+  param1 <- exp(param1) 
+  param2 <- exp(param2)
+  n = sample_size
+  p = c_number
+  
+  ## CONFOUNDERS
+  sigma <- matrix(0,nrow=p,ncol=p); diag(sigma) <- 1
+  # round down the continuous confounders to reduce the number of split points to search over
+  c     <- round(mvtnorm::rmvnorm(n, mean=rep(0,p), sigma=sigma), 3) 
+  
+  # DESIGN MATRIX FOR C 
+  dMat <- model.matrix(
+    as.formula(
+      paste("~(",
+            paste("c[,",1:ncol(c),"]", collapse="+")
+            ,")")
+    )
+  )
+  
+  beta <- rep(log(1.75),c_number) # outcome-confounder ORs:1.75
+  
+  theta <- c(-.5, rep(log(1.5),c_number)) #exposure-confounder ORs:1.5, Q: why intercept -0.5?
+  pi_ <- expit(dMat%*%theta) # predicted exposure and modifier probability (they are the same)
+  
+  # EXPOSURE AND MODIFIER
+  x    <- rbinom(n,1,pi_)
+  m    <- rbinom(n,1,pi_)
+  
+  balancing_intercept <- -log((1/.1)-1) - # outcome mu of .1
+    log(param1)*pi_ -       # offsetting exposure effect
+    log(2)*pi_ +            # offsetting modifier effect: outcome-modifier OR: 2
+    log(param2)*pi_*pi_   # offsetting interaction term
+  
+  # PREDICTED OUTCOME PROBABILITIES
+  mu_y <- expit(balancing_intercept +
+                  log(param1)*x+ log(2)*m - log(param2)*x*m + # this is the exposure and modifier effect
+                  dMat[,-1]%*%beta # this is the confounder-outcome part
+  )
+  
+  y <- rbinom(n, 1, mu_y)
+  
+  dat <- data.frame(y,m,c,x)
+  names(dat) <- c("y","m",paste0("c",1:c_number),"x")
+  
+  # mu_exposure_modifier: Q: what if modifier is also a confounder?
+  mu11 <- expit(balancing_intercept + 
+                  log(param1)*1 + log(2)*1 - 
+                  log(param2)*1*1 + dMat[,-1]%*%beta)
+  
+  mu10 <- expit(balancing_intercept + 
+                  log(param1)*1 + log(2)*0 - 
+                  log(param2)*1*0 + dMat[,-1]%*%beta)
+  
+  mu01 <- expit(balancing_intercept + 
+                  log(param1)*0 + log(2)*1 - 
+                  log(param2)*0*1 + dMat[,-1]%*%beta)
+  
+  mu00 <- expit(balancing_intercept + 
+                  log(param1)*0 + log(2)*0 - 
+                  log(param2)*0*0 + dMat[,-1]%*%beta)
+  # oracle truth in the data generating mechanism
+  rd_m0 <- mean(mu10 - mu00)
+  rd_m1 <- mean(mu11 - mu01)
+  rd_ate <- mean(rd_m0*(1-pi_) + rd_m1*pi_)
+  
+  # mu1 <-  expit(balancing_intercept +
+  #                 log(param1)*1 + log(2)*m -
+  #                 log(param2)*1*m + dMat[,-1]%*%beta)
+  # mu0 <-  expit(balancing_intercept +
+  #                 log(param1)*0 + log(2)*m -
+  #                 log(param2)*0*m + dMat[,-1]%*%beta)
+  # rd_ate <- mean(mu1-mu0)
+  
+  
+  
+  ## DATA GENERATION FINISHED; DATA ANALYSIS START
+  
+  # ORACLE MODEL, G COMP
+  mod_form <- as.formula(paste("y ~ x + m + x*m +", paste0("c", 1:c_number, collapse = "+")))
+  mod_true <- glm(mod_form, data = dat, family = binomial(link = "logit"))
+  
+  # c_matrix1 <- c(0, (as.matrix(attr(summary(mod_true)$terms,"term.labels")) == c("x")))
+  # c_matrix2 <- c_matrix1 + c(0, (as.matrix(attr(summary(mod_true)$terms,"term.labels")) == c("x:m")))
+  # 
+  # ate_m0 <- c(coef(mod_true) %*% c_matrix1, 
+  #             sqrt(t(c_matrix1) %*% vcov(mod_true) %*% c_matrix1))
+  # 
+  # ate_m1 <- c(coef(mod_true) %*% c_matrix2, 
+  #             sqrt(t(c_matrix2) %*% vcov(mod_true) %*% c_matrix2))
+  
+  mu1_gcomp <- mean(predict(mod_true, newdata = transform(dat, x = 1), type = "response"))
+  mu0_gcomp <- mean(predict(mod_true, newdata = transform(dat, x = 0), type = "response"))
+  
+  ate_gcomp <- mu1_gcomp - mu0_gcomp
+  
+  mu11_gcomp <- mean(predict(mod_true, newdata = transform(dat, x = 1, m = 1), type = "response"))
+  mu01_gcomp <- mean(predict(mod_true, newdata = transform(dat, x = 0, m = 1), type = "response"))
+  
+  ate_m1_gcomp <- mu11_gcomp - mu01_gcomp
+  
+  mu10_gcomp <- mean(predict(mod_true, newdata = transform(dat, x = 1, m = 0), type = "response"))
+  mu00_gcomp <- mean(predict(mod_true, newdata = transform(dat, x = 0, m = 0), type = "response"))
+  
+  ate_m0_gcomp <- mu10_gcomp - mu00_gcomp
+  
+  boot_func <- function(data, index){
+    
+    boot_dat <- data[index, ]
+    
+    mod_true_ <- glm(mod_form, data = boot_dat, family = binomial(link = "logit"))
+    
+    mu1_ <- mean(predict(mod_true_, newdata = transform(boot_dat, x = 1), type = "response"))
+    mu0_ <- mean(predict(mod_true_, newdata = transform(boot_dat, x = 0), type = "response"))
+    
+    ate_gcomp_ <- mu1_ - mu0_
+    
+    mu11_ <- mean(predict(mod_true_, newdata = transform(boot_dat, x = 1, m = 1), type = "response"))
+    mu01_ <- mean(predict(mod_true_, newdata = transform(boot_dat, x = 0, m = 1), type = "response"))
+    
+    ate_m1_gcomp_ <- mu11_ - mu01_
+    
+    mu10_ <- mean(predict(mod_true_, newdata = transform(boot_dat, x = 1, m = 0), type = "response"))
+    mu00_ <- mean(predict(mod_true_, newdata = transform(boot_dat, x = 0, m = 0), type = "response"))
+    
+    ate_m0_gcomp_ <- mu10_ - mu00_
+    
+    return(c(ate_gcomp_,
+             ate_m0_gcomp_,
+             ate_m1_gcomp_))
+    
+  }
+  
+  boot_res <- boot(boot_func, data = dat, R = 500)
+  
+  ate_gcomp <- c(ate_gcomp, sd(boot_res$t[,1]))
+  names(ate_gcomp) <- c("estimate", "std.err")
+  
+  ate_m0_gcomp <- c(ate_m0_gcomp, sd(boot_res$t[,2]))
+  names(ate_m0_gcomp) <- c("estimate", "std.err")
+  
+  ate_m1_gcomp <- c(ate_m1_gcomp, sd(boot_res$t[,3]))
+  names(ate_m1_gcomp) <- c("estimate", "std.err")
+  
+  ## use super learner to predict propensity score and outcome for both causal forest and DR learner
+  ## 
+  covariates_matrix <- dat[,c("m", paste0("c", 1:c_number))]
+  covariates_matrix_x <- dat[,c("x", "m", paste0("c", 1:c_number))]
+  outcome <-  as.matrix(dat$y)
+  exposure <- as.matrix(dat$x)
+  
+  # construct a sample size variable 
+  n <- nrow(dat)
+  
+  # We use 10-fold cross fitting, and determine precisely which of the observations get assigned to each fold:
+  num.folds <- 10
+  folds <- sort(seq(n) %% num.folds) + 1
+  
+  mean_learner <- "SL.mean"
+  glm_learner <- "SL.glm"
+  
+  # ranger learner
+  ranger_learner <- "SL.ranger"
+  
+  # glmnet learner
+  glmnet_learner <- "SL.glmnet"
+  
+  # xgboost learner
+  xgboost_learner <- "SL.xgboost"
+  
+  # earth learner
+  #earth_learner <- "SL.earth"
+  
+  sl_lib <- c(ranger_learner,
+              glmnet_learner, 
+              xgboost_learner, 
+              mean_learner,
+              glm_learner)
+  
+  # Specify the number of folds for V-fold cross-validation
+  # Use same folds for causal forest and DR learner
+  # Doing cross-validation this way automatically deploys cross-fitting
+  fold_dat <- tibble(id = 1:n, folds)
+  fold_index <- split(fold_dat$id, fold_dat$folds)
+  
+  fit_mu <- CV.SuperLearner(Y = outcome,
+                            X = covariates_matrix_x, 
+                            method = "method.NNLS", 
+                            family = binomial,
+                            SL.library = sl_lib,
+                            cvControl = list(V = num.folds, validRows = fold_index),
+                            control = list(saveCVFitLibrary = T),
+                            parallel = "seq",
+                            verbose = T)
+  
+  fit_pi <- CV.SuperLearner(Y = exposure,
+                            X = covariates_matrix,
+                            method = "method.NNLS", 
+                            family = binomial,
+                            SL.library = sl_lib,
+                            cvControl = list(V = num.folds, validRows = fold_index),#, stratifyCV = TRUE),
+                            control = list(saveCVFitLibrary = T),
+                            parallel = "seq",
+                            verbose = T)
+  
+  pscore <- as.matrix(fit_pi$SL.predict)
+  
+  mu_hat <- as.matrix(fit_mu$SL.predict)
+  
+  ## HERE WE HAVE CAUSAL FOREST CODE TO IDENTIFY APPROPORATE MODIFERS
+  # we construct a causal forest using the `causal_forest` function from the grf package:
+  forest <- causal_forest(X = covariates_matrix, 
+                          Y = outcome, 
+                          W = exposure, 
+                          Y.hat = mu_hat, # use super learner for w hat and Y hat
+                          W.hat = pscore,
+                          num.trees = 2000,
+                          honesty = TRUE,
+                          #min.node.size = 10, # might need increase this, tunable
+                          #alpha = .05, #  tunable
+                          #imbalance.penalty = 0, #  tunable
+                          stabilize.splits = TRUE,
+                          tune.parameters = "all", # might need change this
+                          tune.num.trees = 200,
+                          tune.num.reps = 50,
+                          tune.num.draws = 1000,
+                          compute.oob.predictions = TRUE,
+                          num.threads = 10,
+                          seed = 123,
+                          clusters = folds) # https://bit.ly/42aBuPP
+  
+  #tau.hat = predict(forest)$predictions
+  
+  ## COMPUTE THE ATE FROM THE CAUSAL FOREST ALGORITHM
+  # we then use the `average_treatment_effect()` function to obtain an ATE estimate
+  cf_ate <- average_treatment_effect(forest)
+  
+  blp_cf <- tidy(best_linear_projection(forest, covariates_matrix)) %>% 
+    filter(term != "(Intercept)") %>% 
+    arrange(desc(abs(statistic))) %>% 
+    mutate(parameter1 = param1,
+           parameter2 = param2,
+           sample_size = n,
+           confounder_number = c_number,
+           seed_number = seed_value,
+           method = "BLP_causal_forest",
+           group = group)
+  ## need to pick the "strong" ones from this, and store the results
+  ## what we should do is rank the |test statistic| and pick the first one to check if it's m
+  ## maybe keep this whole thing??
+  # if(first(blp_cf$term)=="m"){
+  #   m_cf = 1
+  # } else {
+  #   m_cf = 0
+  # }
+  
+  aipw_scores <- get_scores(forest,
+                            subset = NULL,
+                            debiasing.weights = NULL,
+                            num.trees.for.weights = 2000
+  )
+  
+  fmla <- as.formula(aipw_scores ~ factor(m)) # Q: why not use tau.hat?
+  score_reg <- lm(fmla, 
+                  data=transform(dat, aipw_scores=aipw_scores))
+  
+  c_matrix1 <- c(1, 0)
+  c_matrix2 <- c(1, 1)
+  
+  cf_cate_m0 <- data.frame(t(c(coef(score_reg) %*% c_matrix1, 
+                               sqrt(t(c_matrix1) %*% vcovHC(score_reg, type = "HC3") %*% c_matrix1))
+  ))
+  names(cf_cate_m0) <- c("estimate", "std.err")
+  
+  cf_cate_m1 <- data.frame(t(c(coef(score_reg) %*% c_matrix2, 
+                               sqrt(t(c_matrix2) %*% vcovHC(score_reg, type = "HC3") %*% c_matrix2))
+  ))
+  names(cf_cate_m1) <- c("estimate", "std.err")
+  
+  # DO THE EXACT SAME FOR DRLEARNER.
+  
+  mu_hat1 <- NULL
+  for(i in 1:num.folds){
+    mu_hat1 <- rbind(mu_hat1, 
+                     predict(fit_mu$AllSL[[i]],
+                             newdata = base::transform(
+                               covariates_matrix_x[fold_index[[i]],], x = 1), 
+                             onlySL=T)$pred)
+  }
+  
+  mu_hat0 <- NULL
+  for(i in 1:num.folds){
+    mu_hat0 <- rbind(mu_hat0, 
+                     predict(fit_mu$AllSL[[i]],
+                             newdata = base::transform(
+                               covariates_matrix_x[fold_index[[i]],], x = 0), 
+                             onlySL=T)$pred)
+  }
+  
+  ## aipw
+  aipw_func <- function(exposure, outcome, pscore, mu_hat, mu_hat0, mu_hat1){
+    aipw_score <- ((2*exposure - 1)*(outcome - mu_hat))/((2*exposure - 1)*pscore + (1 - exposure)) + (mu_hat1 - mu_hat0)
+    return(aipw_score)
+  }
+  
+  aipw_score <- aipw_func(exposure, 
+                          outcome, 
+                          pscore, 
+                          mu_hat, 
+                          mu_hat0, 
+                          mu_hat1)
+  colnames(aipw_score) <- NULL
+  
+  aipw_psi <- mean(aipw_score)
+  
+  aipw_se <- sd(aipw_score)/sqrt(n)
+  
+  aipw_ate <- data.frame(t(c(aipw_psi, aipw_se)))
+  names(aipw_ate) <- c("estimate", "std.err")
+  
+  ## best linear projection
+  score_dat <- data.frame(aipw_score, covariates_matrix)
+  score_fit <- lm(aipw_score ~ ., data = score_dat)
+  
+  blp_aipw <- coeftest(score_fit, vcov = vcovHC(score_fit, type = "HC3"))
+  
+  blp_drl <- tidy(blp_aipw) %>% 
+    filter(term != "(Intercept)") %>% 
+    arrange(desc(abs(statistic))) %>% 
+    mutate(parameter1 = param1,
+           parameter2 = param2,
+           sample_size = n,
+           confounder_number = c_number,
+           seed_number = seed_value,
+           method = "BLP_dr_learner",
+           group = group) 
+  
+  
+  # if(first(blp_drl$term)=="m"){
+  #   m_drl = 1
+  # } else {
+  #   m_drl = 0
+  # }
+  
+  fmla <- as.formula(aipw_score ~ factor(m))
+  score_reg <- lm(fmla, 
+                  data=transform(dat, aipw_scores=aipw_score))
+  
+  c_matrix1 <- c(1, 0)
+  c_matrix2 <- c(1, 1)
+  
+  drl_cate_m0 <- data.frame(t(c(coef(score_reg) %*% c_matrix1, 
+                                sqrt(t(c_matrix1) %*% vcovHC(score_reg, type = "HC3") %*% c_matrix1))
+  ))
+  names(drl_cate_m0) <- c("estimate", "std.err")
+  
+  drl_cate_m1 <- data.frame(t(c(coef(score_reg) %*% c_matrix2, 
+                                sqrt(t(c_matrix2) %*% vcovHC(score_reg, type = "HC3") %*% c_matrix2))
+  ))
+  names(drl_cate_m1) <- c("estimate", "std.err")
+  
+  # objects to save:
+  result <- rbind(
+    data.frame(estimator = "oracle_ate",            estimate=rd_ate, std.err=NA),
+    data.frame(estimator = "oracle_cate_m0",        estimate=rd_m0, std.err=NA),
+    data.frame(estimator = "oracle_cate_m1",        estimate=rd_m1, std.err=NA),
+    data.frame(estimator = "gComp_ate",             t(ate_gcomp)),
+    data.frame(estimator = "gComp_cate_m0",         t(ate_m0_gcomp)),
+    data.frame(estimator = "gComp_cate_m1",         t(ate_m1_gcomp)),
+    data.frame(estimator = "causal_forest_ate",     t(cf_ate)),
+    data.frame(estimator = "causal_forest_cate_m0", cf_cate_m0),
+    data.frame(estimator = "causal_forest_cate_m1", cf_cate_m1),
+    data.frame(estimator = "DRLearner_ate",         aipw_ate),
+    data.frame(estimator = "DRLearner_cate_m0",     drl_cate_m0),
+    data.frame(estimator = "DRLearner_cate_m1",     drl_cate_m1)
+    ) %>%
+    mutate(group = group, seed_number = seed_value, sample_size = n, confounder_number = c_number, or_exposure = param1, or_interaction = param2, p_exposure = mean(pi_)) %>%
+    select(group, estimator, seed_number, sample_size, confounder_number, or_exposure, or_interaction, p_exposure, estimate, std.err)
+  ## writing files for each group of parameters:
+  write_csv(blp_cf, here("output",paste0("Blp_cf_c", c_number, "_n", n, "_sims", number_sims, ".csv")), append = T)
+  write_csv(blp_drl, here("output",paste0("Blp_drl_c", c_number, "_n", n, "_sims", number_sims, ".csv")), append = T)
+  write_csv(result, here("output",paste0("All_results_c", c_number, "_n", n, "_sims", number_sims, ".csv")), append = T)
+  return(list(result, blp_cf, blp_drl))
+  
+}   
---
+++ code/run_cluster_sim.R
@@ -0,0 +1,157 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# run the function of cluster_sim using foreach with doParallel in HPC
+
+packages <- c("data.table","tidyverse","SuperLearner","boot",
+              "here","mvtnorm","foreach","doParallel","broom",
+              "ggplot2","grf", "lmtest", "sandwich", "glmnet") # added a package
+
+for (package in packages) {
+  library(package, character.only=T)
+}
+
+func.packages <- c("data.table","tidyverse","SuperLearner","boot",
+                   "here","mvtnorm","broom", "grf", "lmtest", "sandwich", "glmnet")
+
+c_number <- as.numeric(args[1])
+cat(paste("Number of Confounders for Each Sim:", c_number), "\n")
+sample_size <- as.numeric(args[2])
+cat(paste("Sample Size for Each Sim:", sample_size), "\n")
+number_sims <- as.numeric(args[3])
+cat(paste("Number of Simulations:", number_sims, "\n"))
+
+# read in the seed data
+seed_data <- read_csv(
+  here("data", "random_seed_values.csv")
+)
+
+random_seeds <- seed_data$random_seeds
+
+# read in the truth file
+truth_data <- read_csv(
+  here("data", "truth_data_simulation_all.csv")
+)
+
+# use c_number to extract the parameters needed from the truth data
+truth_data <- truth_data %>%
+  filter(c_dim == c_number) %>%
+  mutate(group = row_number())
+
+# param_dat1 <- list(param1 = 1.29,
+#                   param2 = c(1.05, 1.29, 1.6))
+# 
+# data_grid1 <- expand.grid(param_dat1, KEEP.OUT.ATTRS = FALSE)
+# 
+# param_dat2 <- list(param1 = 1,
+#                    param2 = c(.815, 1, 1.24))
+# 
+# data_grid2 <- expand.grid(param_dat2, KEEP.OUT.ATTRS = FALSE)
+# 
+# param_dat3 <- list(param1 = 0.765,
+#                    param2 = c(.625, .765, .945))
+# 
+# data_grid3 <- expand.grid(param_dat3, KEEP.OUT.ATTRS = FALSE)
+# 
+# data_grid <- rbind(data_grid1, data_grid2, data_grid3)
+
+source("cluster_sim_func.R")
+
+# save the random seed file
+# set.seed(123)
+# random_seeds <- runif(1000, min = 1, max = 249999)
+# random_seeds <- unique(round(random_seeds, digits = 0))
+# write_csv(data.frame(random_seeds), here("data","random_seed_values.csv"))
+
+#parallel::detectCores()
+
+#n.cores <- parallel::detectCores() - 2 # the detected number of cores might not be active on HPC
+
+n.cores = 20
+
+my.cluster <- parallel::makeCluster(
+  n.cores, 
+  type = "PSOCK" # qz: FORK doesn't work on windows,doesn't work for Rollins cluster
+)
+
+print(my.cluster)
+
+doParallel::registerDoParallel(cl = my.cluster)
+
+foreach::getDoParRegistered()
+
+
+results <- foreach(i = 1:nrow(truth_data), .packages = func.packages) %dopar% {
+  
+  cat(paste("for loop iteration",i), "\n")
+  
+  result_list <- lapply(random_seeds[1:number_sims], function(x) cluster_sim(seed_value = x,
+                                                                        number_sims = number_sims,
+                                                                        sample_size = sample_size, 
+                                                                        c_number = c_number,
+                                                                        param1 = log(truth_data[i,]$param1), 
+                                                                        param2 = log(truth_data[i,]$param2),
+                                                                        group = i))
+  
+}
+
+parallel::stopCluster(cl = my.cluster)
+
+# extract the blp output and result table to separate files
+result_all <- blp_cf_all <- blp_drl_all <- NULL
+for (i in seq(nrow(truth_data))) {
+  for (j in seq(number_sims)) {
+    result_all <- rbind(result_all, results[[i]][[j]][[1]])
+    blp_cf_all <- rbind(blp_cf_all, results[[i]][[j]][[2]])
+    blp_drl_all <-rbind(blp_drl_all, results[[i]][[j]][[3]])
+  }
+}
+
+
+# combine the truth with the result
+truth <- gather(truth_data, estimand, truth, risk_diff, risk_diff_m0, risk_diff_m1) %>%
+  mutate(type = ifelse(estimand == "risk_diff", "ate", ifelse(estimand == "risk_diff_m0", "_m0", "_m1"))) %>%
+  select(group, truth, type)
+result_all_truth <- result_all %>%
+  mutate(type = substr(estimator, nchar(estimator)-2, nchar(estimator))) %>%
+  left_join(truth, by = c("type", "group"))
+
+# calculate the bias, MSE and coverage probability compared with truth
+summary_truth <- result_all_truth %>%
+  mutate(bias = estimate - truth, 
+         coverage = ifelse(estimate - 1.96*std.err <= truth & truth <= estimate + 1.96*std.err, 1, 0)) %>%
+  group_by(group, estimator) %>%
+  mutate(p_exposure_mean = mean(p_exposure), bias_mean = mean(bias), MSE = mean((bias)^2), coverage_prob = mean(coverage)) %>%
+  select(group, estimator, sample_size, confounder_number, or_exposure, or_interaction, p_exposure_mean, truth, bias_mean, MSE, coverage_prob) %>%
+  unique()
+ 
+# combine the oracle truth with the result
+oracle <- result_all_truth %>%
+  filter(substr(estimator, 1, 6) == "oracle") %>%
+  select(group,  seed_number, estimator, estimate, type) %>% 
+  dplyr::rename(oracle = estimate) %>%
+  select(group, seed_number, oracle, type)
+result_all_oracle <- result_all %>%
+  filter(substr(estimator, 1, 6) != "oracle") %>%
+  mutate(type = substr(estimator, nchar(estimator)-2, nchar(estimator))) %>%
+  left_join(oracle, by = c("type", "group", "seed_number"))
+# calculate the bias, MSE and coverage probability compared with oracle
+summary_oracle <- result_all_oracle %>%
+  mutate(bias = estimate - oracle, 
+         coverage = ifelse(estimate - 1.96*std.err <= oracle & oracle <= estimate + 1.96*std.err, 1, 0)) %>%
+  group_by(group, estimator) %>%
+  mutate(p_exposure_mean = mean(p_exposure), oracle_mean = mean(oracle), bias_mean = mean(bias), MSE = mean((bias)^2), coverage_prob = mean(coverage))%>%
+  select(group, estimator, sample_size, confounder_number, or_exposure, or_interaction, p_exposure_mean, oracle_mean, bias_mean, MSE, coverage_prob) %>%
+  unique()
+
+
+# save the files
+## all results
+write_csv(result_all, here("output", paste0("All_results_c", c_number, "_n", sample_size, "_sims", number_sims, "_", Sys.Date(), ".csv")))
+## summary results
+write_csv(summary_truth, here("output", paste0("Summary_truth_c", c_number, "_n", sample_size, "_sims", number_sims, "_", Sys.Date(), ".csv")))
+write_csv(summary_oracle, here("output", paste0("Summary_oracle_c", c_number, "_n", sample_size, "_sims", number_sims, "_", Sys.Date(), ".csv")))
+## blp results
+write_csv(blp_cf_all, here("output", paste0("Blp_cf_c", c_number, "_n", sample_size, "_sims", number_sims, "_", Sys.Date(), ".csv")))
+write_csv(blp_drl_all, here("output", paste0("Blp_drl_c", c_number, "_n", sample_size, "_sims", number_sims, "_", Sys.Date(), ".csv")))
+
